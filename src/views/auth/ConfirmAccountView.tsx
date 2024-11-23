b04c38e42eafe3379491d10073a47940043149b1
JhairDev05 <jhairdev05@gmail.com>
2024-11-23 16:50:13 -0600

    fix

--- src/views/auth/ConfirmAccountView.tsx
+++ src/views/auth/ConfirmAccountView.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
 import { PinInput, PinInputField } from '@chakra-ui/pin-input'
 import { useMutation } from '@tanstack/react-query'
 import { ConfirmToken } from "@/types/index";
@@ -9,6 +9,7 @@ import { toast } from "react-toastify";
 export default function ConfirmAccountView() {
     const [token, setToken] = useState<ConfirmToken['token']>('')
 
+    const navigate = useNavigate()
     const { mutate } = useMutation({
         mutationFn: confirmAccount,
         onError: (error) => {
@@ -16,6 +17,9 @@ export default function ConfirmAccountView() {
         },
         onSuccess: (data) => {
             toast.success(data)
+            setTimeout(() => {
+                navigate('/')
+            }, 2000);
         }
     })
 
